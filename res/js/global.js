24860ad50a34642212a3043ca11a96b4c5ab1d1a
JacobLinCool <jacoblincool@gmail.com>
2020-11-28 21:31:18 +0800

    fix

--- res/js/global.js
+++ res/js/global.js
@@ -1,17 +1,35 @@
 // Curtain
 let curtain = new Curtain();
+curtain.text("Jacob Lin's Website");
+document.addEventListener("DOMContentLoaded", () => {curtain.progress(25)});
+document.addEventListener("load", () => {curtain.out()});
 function Curtain() {
-    let curtain = ".first_curtain", words = ".curtain_words", bar = ".curtain_bar";
+    let curtain = ".first_curtain", words = ".curtain_words", words_bg = ".curtain_words_bg", bar = ".curtain_bar";
     let self = this;
-    self.progress = function(n) {
+    self.progress = async function(n) {
         if(typeof n != "number") n = 0;
         if(n < 0) n = 0;
         if(n <= 1) n *= 100;
         if(n > 100) n = 100;
         document.querySelector(bar).style.width = n + "%";
+        document.querySelector(words).style.width = parseFloat(getComputedStyle(document.querySelector(words_bg)).getPropertyValue("width"))*n/100 + "px";
         if(n == 100) {
-            document.querySelector(curtain).style.animation = "curtain_out 1s";
+            await wait(600);
+            document.querySelector(curtain).classList.add("removed");
         }
     };
     self.out = function() { self.progress(1) };
+    self.text = function(s) {
+        document.querySelector(words).innerHTML = s;
+        document.querySelector(words_bg).innerHTML = s;
+    }
+}
+
+// Async Wait
+function wait(t=1000) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(t);
+        }, t);
+    });
 }
