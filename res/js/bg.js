b63bdb469a7229664038d299f1525ea0cb13a8ea
JacobLinCool <jacoblincool@gmail.com>
2020-12-01 18:22:20 +0800

    add

--- res/js/bg.js
+++ res/js/bg.js
@@ -1,8 +1,13 @@
-function bg() {
-    let self = this;
-    self.style = document.createElement("style");
-    
-    self.t = function() {
+(async function() {
+    await preparations.bg_html;
+})()
 
+
+function bg(elm=document.querySelector(".background")) {
+    let self = this;
+    self.elm = elm;
+
+    self.t = function(a=1, b=0, c=0, d=1, e=0, f=0) {
+        self.elm.style.transform = `matrix(${a}, ${b}, ${c}, ${d}, ${e}, ${f})`;
     };
 }
--- res/js/global.js
+++ res/js/global.js
@@ -1,16 +1,19 @@
 // Curtain
 let curtain = new Curtain();
 curtain.text("Jacob Lin's Website");
-document.addEventListener("DOMContentLoaded", async () => {curtain.progress(25)});
-window.addEventListener("load", async () => {curtain.out()});
+document.addEventListener("DOMContentLoaded", async () => {curtain.add(25)});
+window.addEventListener("load", async () => {curtain.add(25)});
+
 function Curtain() {
     let curtain = ".first_curtain", words = ".curtain_words", words_bg = ".curtain_words_bg", bar = ".curtain_bar";
     let self = this;
+    let p = 0;
     self.progress = async function(n) {
         if(typeof n != "number") n = 0;
         if(n < 0) n = 0;
         if(n <= 1) n *= 100;
         if(n > 100) n = 100;
+        p = n;
         document.querySelector(bar).style.width = n + "%";
         document.querySelector(words).style.width = parseFloat(getComputedStyle(document.querySelector(words_bg)).getPropertyValue("width"))*n/100 + "px";
         if(n == 100) {
@@ -18,11 +21,33 @@ function Curtain() {
             document.querySelector(curtain).classList.add("removed");
         }
     };
+    self.add = async function(n) {
+        if(typeof n != "number") n = 0;
+        p += n;
+        return self.progress(p);
+    };
     self.out = function() { self.progress(1) };
     self.text = function(s) {
         document.querySelector(words).innerHTML = s;
         document.querySelector(words_bg).innerHTML = s;
     }
+}
+
+// Background
+(async function() {
+    let bg_html = await preparations.bg_html;
+    document.body.innerHTML += bg_html;
+    let BG = new bg;
+    curtain.add(50);
+})();
+
+function bg(elm=document.querySelector(".background")) {
+    let self = this;
+    self.elm = elm;
+
+    self.t = function(a=1, b=0, c=0, d=1, e=0, f=0) {
+        self.elm.style.transform = `matrix(${a}, ${b}, ${c}, ${d}, ${e}, ${f})`;
+    };
 }
 
 // Async Wait
