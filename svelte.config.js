7af23a26146ee3155b60230e5681bab7fc542f62
JacobLinCool <jacoblincool@gmail.com>
2022-12-28 00:06:02 +0800

    feat: make links external

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,10 @@
 import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 import { mdsvex } from "mdsvex";
+import remark_gfm from "remark-gfm";
+import remark_github from "remark-github";
 import add_classes from "rehype-add-classes";
+import external_links from "rehype-external-links";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,13 +12,13 @@ const config = {
 		vitePreprocess(),
 		mdsvex({
 			extensions: [".md"],
+			remarkPlugins: [
+				remark_gfm,
+				[remark_github, { repository: "jacoblincool/jacoblincool.github.io" }],
+			],
 			rehypePlugins: [
-				[
-					add_classes,
-					{
-						"*": "markdown",
-					},
-				],
+				[add_classes, { "*": "markdown" }],
+				[external_links, { target: "_blank", rel: ["noopener", "noreferrer"] }],
 			],
 		}),
 	],
